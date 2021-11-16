84306043c749c0d7cd978f4a75ce19062488a4be
aldolim66 <90307240+aldolim66@users.noreply.github.com>
2021-11-16 12:40:59 +0800

    Add files via upload

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    private AudioSource audio;
+    public Slider slide;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        audio.volume = slide.value;
+    }
+
+}
+
