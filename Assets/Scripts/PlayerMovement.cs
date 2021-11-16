dcbdfa1c568cc003075f3eef3b37e65dc814501f
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-11-16 20:26:03 +0700

    Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Menentukan Speed
+    [SerializeField] private float speed;
+    //Speed untuk lompat
+    [SerializeField] private float jump;
+    private Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Untuk bisa berjalan ke kiri dan ke kanan
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        // Agar dapat melompat dengan mempertahankan kecepatan horizontal
+        if (Input.GetKey(KeyCode.Space))
+            body.velocity = new Vector2(body.velocity.x, jump);
+    }
+}
