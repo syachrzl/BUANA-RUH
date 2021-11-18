2a12667ea14e3a579354f629d17e9fa230a8ba34
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-11-18 17:41:19 +0700

    Camera Movement follow Player

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,13 +2,25 @@
 
 public class CameraController : MonoBehaviour
 {
+    //Kamera Ruangan
     [SerializeField] private float speed;
     private float currentPostX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow Player
+    [SerializeField] private Transform player;
+    [SerializeField] private float aheadDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPostX, transform.position.y, transform.position.z),ref velocity, speed );
+        //Kamera Ruangan
+        ///transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPostX, transform.position.y, transform.position.z),ref velocity, speed );
+
+        //Follow Player
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
