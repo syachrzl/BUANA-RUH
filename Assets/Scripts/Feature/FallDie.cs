f063bdc908ecd820bd302068722aa0b4981a1e5a
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-11-24 19:31:49 +0700

    Feature fall

---
+++ Assets/Scripts/Feature/FallDie.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDie : MonoBehaviour
+{
+
+    [SerializeField] private float damage;
+    [SerializeField] private Transform player;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
