8c9a85e88e0b45a38e8c46be5eccb14ae638c965
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-11-28 17:12:03 +0700

    Initial Commit

---
+++ Assets/Scripts/Puzzle/Puzzle.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Puzzle : MonoBehaviour
+{
+    [SerializeField] private float startingPuzzle;
+    [SerializeField] private float startEmptyPuzzle;
+      private Animator anim;
+    private bool dead;
+
+
+    public float currentPuzzle { get; private set; }
+    public float EmptyPuzzle { get; private set; }
+
+
+    private void Awake()
+    {
+        currentPuzzle = startingPuzzle;
+        EmptyPuzzle = startEmptyPuzzle;
+        anim = GetComponent<Animator>();
+    }
+
+    public void AddPuzzle(float _value)
+    {
+        currentPuzzle = Mathf.Clamp(currentPuzzle + _value, 0, startEmptyPuzzle);
+
+        if (EmptyPuzzle == 5)
+        {
+            //Light Keluar
+        }
+    }
+
+
+    public void TakePuzzle(float _damage)
+    {
+        currentPuzzle = Mathf.Clamp(currentPuzzle - _damage, 0, startEmptyPuzzle);
+    }
+
+     private void Update()
+    {
+       // if (Input.GetKeyDown(KeyCode.E))
+           // TakePuzzle(1);
+     }
+}
---
+++ Assets/Scripts/Puzzle/PuzzleCollectible.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PuzzleCollectible : MonoBehaviour
+{
+    [SerializeField] private float puzzleValue;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Puzzle>().AddPuzzle(puzzleValue);
+            gameObject.SetActive(false);
+        }
+    }
+
+}
