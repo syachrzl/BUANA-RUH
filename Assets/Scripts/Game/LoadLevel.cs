77d56d0a6e6105124954b51dee63cf72b9259197
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-12-01 09:58:52 +0700

    Fix level 1E

---
+++ Assets/Scripts/Game/LoadLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField] private int Level;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(Level);
+        }
+    }
+}
---
+++ Assets/Scripts/Ghost/Ghost.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class Ghost : MonoBehaviour
+{
+    public AIPath aiPath;
+    [SerializeField] private float damage;
+    [SerializeField] private Transform player;
+
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (aiPath.desiredVelocity.x < -0.01f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+}
