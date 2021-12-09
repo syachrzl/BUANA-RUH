2e70682941f08ec52e108e12090357a1f591c09f
Syah Rizal Fauzy <syachrzl@gmail.com>
2021-12-09 09:06:19 +0700

    Poles level 1 dan 2

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -13,7 +13,7 @@ public class PlayerManager : MonoBehaviour
     private void Awake()
     {
         isGameOver = false;
-        //GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos; 
+        GameObject.FindGameObjectWithTag("Player").transform.position = lastCheckPointPos; 
     }
 
     // Start is called before the first frame update
